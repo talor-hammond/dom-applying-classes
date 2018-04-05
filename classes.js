1bfbc442762d1efb2ab59748e5984d81a2a9c9fd
talor-hammond <talorjhammond@gmail.com>
2018-04-06 02:55:21 +1200

    change colour of div two to green

--- classes.js
+++ classes.js
@@ -4,10 +4,11 @@
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
-  // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one()
     
   // Your turn! Create a new function called `two`, then call it from here.
+  two()
+
 }
 
 function one () {
@@ -19,6 +20,13 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two() {
+
+  var two = document.getElementById('two')
+
+  two.classList.add('green')
+
+}
 
 // CREATE FUNCTION three HERE
 
